8070c6b0c7ec3ebe6fe5d0c43c5ac8805cfdc3f2
iamstan13y <iamstan13y@gmail.com>
2022-03-20 01:18:43 +0200

    Get All Donations.

--- Services/Donations.API/Program.cs
+++ Services/Donations.API/Program.cs
@@ -1,4 +1,5 @@
 using Donations.API.Models.Data;
+using Donations.API.Models.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IDonationRepository, DonationRepository>();
 builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
