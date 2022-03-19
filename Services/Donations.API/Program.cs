21502d2cf508c04a6c9291352da3c42e4d16efbd
iamstan13y <iamstan13y@gmail.com>
2022-03-20 00:19:28 +0200

    DB configs.

--- Services/Donations.API/Program.cs
+++ Services/Donations.API/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
