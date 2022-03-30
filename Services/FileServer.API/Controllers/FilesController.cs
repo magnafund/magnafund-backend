fde90ab4e6e1895a34e05b9e3f20ad8b4650b081
iamstan13y <iamstan13y@gmail.com>
2022-03-30 17:47:23 +0200

    File Controller.

---
+++ Services/FileServer.API/Controllers/FilesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        [HttpPost("upload-file")]
+        public async Task UploadFileAsync(IFormFile file)
+        {
+
+        }
+    }
+}
