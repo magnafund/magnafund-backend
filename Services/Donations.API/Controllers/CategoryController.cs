6a031361560f02316cb8c42885ef9b1d97bb9bf1
iamstan13y <iamstan13y@gmail.com>
2022-03-31 10:36:36 +0200

    Category controller & models.

---
+++ Services/Donations.API/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Donations.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddCategory()
+        {
+
+        }
+    }
+}
---
+++ Services/Donations.API/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Donations.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
