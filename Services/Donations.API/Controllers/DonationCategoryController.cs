3a1a86ef92da801f2ecae3ec335507a5899259b5
iamstan13y <iamstan13y@gmail.com>
2022-03-31 13:11:09 +0200

    API Version.

--- Services/Donations.API/Controllers/DonationCategoryController.cs
+++ Services/Donations.API/Controllers/DonationCategoryController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Donations.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class DonationCategoryController : ControllerBase
     {
